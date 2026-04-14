cbed8585b664fab8e8436c15ec36cddb876ce642
rishabhkashyap01 <rishabhkashyap8135@gmail.com>
2026-04-14 23:55:50 +0530

    test added

---
+++ test.py
@@ -0,0 +1,23 @@
+import numpy as np
+from decision_tree import DecisionTree
+
+# Sample dataset
+X = np.array([
+    [2.3, 1.5],
+    [1.3, 3.5],
+    [3.3, 2.5],
+    [2.0, 2.0]
+])
+
+y = np.array([0, 1, 0, 1])
+
+# Create model
+clf = DecisionTree(max_depth=3, criterion="entropy")
+
+# Train
+clf.fit(X, y)
+
+# Predict
+predictions = clf.predict(X)
+
+print("Predictions:", predictions)
